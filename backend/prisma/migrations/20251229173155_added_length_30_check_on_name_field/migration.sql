8b95aa64e44cdbbdeeedb63eb3ad189d13a0bb6c
See-Its-Manish <seeitsmanish@gmail.com>
2025-12-31 01:20:14 +0530

    impr(room-schema): restricted room length to 30 chars only

---
+++ backend/prisma/migrations/20251229173155_added_length_30_check_on_name_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `name` on the `Room` table. The data in that column could be lost. The data in that column will be cast from `VarChar(50)` to `VarChar(30)`.
+
+*/
+-- AlterTable
+ALTER TABLE "Room" ALTER COLUMN "name" SET DATA TYPE VARCHAR(30);
